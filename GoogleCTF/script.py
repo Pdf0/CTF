75386931c05941fbd57098aee1e184570792a886
Pedro Sousa <pedro.er.sousa@gmail.com>
2023-02-19 19:48:40 +0000

    Add first GoogleCTF writeups

---
+++ GoogleCTF/script.py
@@ -0,0 +1,19 @@
+     
+     
+p = []
+
+p.append(52037)
+p.append(52077)
+p.append(52077)
+p.append(52066)
+p.append(52046)
+p.append(52063)
+p.append(52081)
+p.append(52081)
+p.append(52085)
+p.append(52077)
+p.append(52080)
+p.append(52066)
+
+for num in p:
+    print(chr(num - 0xCafe), end="")
